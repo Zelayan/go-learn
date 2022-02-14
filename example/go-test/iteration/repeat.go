484d07dd3e9bc4643b8df968a38f358cb0e4714d
zeze <zeze@zezedeMacBook-Pro.local>
2022-02-14 21:23:58 +0800

    feat:go test

---
+++ example/go-test/iteration/repeat.go
@@ -0,0 +1,12 @@
+package iteration
+
+const repeatCount = 5
+
+func Repeat(character string, count int) string {
+
+	var repeated string 
+	for i := 0; i < count; i++ {
+		repeated += character
+	}
+	return repeated
+}
