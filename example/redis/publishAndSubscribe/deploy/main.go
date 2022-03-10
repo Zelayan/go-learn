b266eaecd3e880658b2f120a59b21da60aa9925b
Zelayan <chenzezeya@163.com>
2022-03-10 17:13:26 +0800

    feat:add redis

--- example/redis/publishAndSubscribe/deploy/main.go
+++ example/redis/publishAndSubscribe/deploy/main.go
@@ -1,20 +1,27 @@
 package deploy
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"io"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
 func Deploy() error {
 
 	deployDone := make(chan struct{})
-	runShDone := make(chan struct{})
+	runShDone := make(chan struct{}, 1)
 
 	go subscribe(deployDone)
+
 	time.Sleep(5 * time.Second)
 	runShDone <- struct{}{}
+
+	ticker := time.NewTicker(10 * time.Second)
 
 	select {
 	case <-deployDone:
@@ -23,7 +30,12 @@ func Deploy() error {
 	case <-runShDone:
 		fmt.Println("deploy done")
 		return nil
+	case <- ticker.C:
+		fmt.Println("ticker")
+		return nil
 	}
+
+	return nil
 
 }
 
@@ -57,3 +69,63 @@ func subscribe(ch chan struct{}) {
 		ch <- struct{}{}
 	}
 }
+
+const TRACEFILE = "/Users/chenze/tmp/1"
+const TRACEFILEDir = "/Users/chenze/tmp"
+
+func GetDeployProcess() error{
+	file, err := os.Open(TRACEFILE)
+	if err != nil {
+		return fmt.Errorf("not in deploy:%w", err)
+	}
+
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				time.Sleep(100 * time.Millisecond)
+			} else {
+				break
+			}
+		}
+		fmt.Print(string(line))
+	}
+	return nil
+}
+func ReadFilee(filepath string) (string, error){
+	content ,err :=ioutil.ReadFile(filepath)
+	if err !=nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func ReadFile(path string) (bool, error){
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+func ReadLine(lineNumber int, path string) (string, int) {
+	file, _ := os.Open(path)
+	fileScanner := bufio.NewScanner(file)
+	lineCount := 1
+
+	result := ""
+	for fileScanner.Scan(){
+		if lineCount == lineNumber{
+			result =  fileScanner.Text()
+		}
+		lineCount++
+
+	}
+	defer file.Close()
+	return result, lineCount - 1
+}
+
--- example/redis/publishAndSubscribe/deploy/main_test.go
+++ example/redis/publishAndSubscribe/deploy/main_test.go
@@ -3,6 +3,8 @@ package deploy
 import (
 	"context"
 	"github.com/go-redis/redismock/v8"
+	"net/http"
+	"os"
 	"testing"
 	"time"
 )
@@ -10,6 +12,29 @@ import (
 func TestDeploy(t *testing.T) {
 
 	Deploy()
+}
+
+func TestSercer(t *testing.T) {
+
+	http.HandleFunc("/deploy", func(writer http.ResponseWriter, request *http.Request) {
+
+		err := Deploy()
+		if err != nil {
+
+		}
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("xxxx"))
+	})
+	http.HandleFunc("/test", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		filee, err := ReadFilee(TRACEFILE)
+		if err != nil {
+
+		}
+		writer.Write([]byte(filee))
+	})
+
+	http.ListenAndServe(":1000", nil)
 }
 
 func TestMockRedis(t *testing.T) {
@@ -77,4 +102,33 @@ func TestTTRedisClient(t *testing.T) {
 
 	t.Fatal("xxx")
 	t.Log("aaaaa")
+}
+
+func TestName(t *testing.T) {
+	content, err := ReadFilee(TRACEFILE)
+	if err != nil {
+
+	} else {
+		t.Log(content)
+	}
+}
+func TestReadLine(t *testing.T) {
+	var current = 5
+	line, cont := ReadLine(current, TRACEFILE)
+	if line != "" {
+		t.Log(line)
+	}
+	t.Logf("count:%d", cont)
+	t.Logf("current:%d", current)
+}
+
+func TestCreatfile(t *testing.T) {
+
+	create, err := os.Create(TRACEFILE)
+	if err != nil {
+		 os.MkdirAll(TRACEFILEDir, os.ModePerm)
+		create, _ = os.Create(TRACEFILE)
+	}
+	create.WriteString("2xxx\n")
+
 }
