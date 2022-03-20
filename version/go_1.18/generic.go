af359e5e5aa087f964188657a1793d19ad2b3d26
zeze <chenzezeya@gmail.com>
2022-03-20 21:17:19 +0800

    fff

--- version/go_1.18/generic.go
+++ version/go_1.18/generic.go
@@ -1,10 +1,14 @@
 package generic
 
-func d()  {
-	
+func Add[T int | int64](a, b T) T {
+	return a + b
 }
-
-func Add[T int|int64](a, b T) T {
-     return a + b
+func Sub[T int | int64](a, b T) T {
+	return a - b
 }
-
+func Mulit[T int | int64](a, b T) T {
+	return a * b
+}
+func SS() {
+	ch := make()
+}
--- version/go_1.18/generic_test.go
+++ version/go_1.18/generic_test.go
@@ -8,3 +8,9 @@ import (
 func TestAdd(t *testing.T) {
 	fmt.Println("1 + 2 =", Add[int64](1, 2))
 }
+func TestDel(t *testing.T) {
+	t.Log(Add[int64](1, 2))
+}
+func TestTest(t *testing.T) {
+	t.Log("xxx")
+}
