a1218e12db7cf83195707970516a7cb7ee0ed954
zeze <chenzezeya@gmail.com>
2022-07-22 23:22:54 +0800

    feat:add panic

---
+++ example/go-panic/main.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+/*func main() {
+	recover()
+	panic("it is panic") // not recover
+}
+*/
+
+/*func main() {
+	defer func() {
+		recover()
+	}()
+
+	panic("it is panic") // recover
+}
+*/
+/*
+func main() {
+	defer recover()
+	panic("it is panic") // no recover
+}*/
+
+/*func main() {
+	defer func() {
+		defer func() {
+			recover()
+		}()
+	}()
+
+	panic("it is panic") // not recover
+}
+*/
+
+/*func main() {
+	defer doRecover()
+	panic("it is panic") // recover
+}
+
+func doRecover() {
+	defer recover()
+}
+*/
+
+/*func main() {
+	defer doRecover()
+	panic("it is panic") // recover
+}
+
+func doRecover() {
+	recover()
+	fmt.Println("hello")
+}*/
+
+/*func main() {
+	defer func() {
+		defer func() {
+			recover()
+		}()
+	}()
+
+	panic("it is panic") // not recover
+}*/
+
+func main() {
+
+	go test()
+	fmt.Println("main done")
+}
+
+func test() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("recovery:%v", err)
+		}
+	}()
+	panic("xx")
+}
