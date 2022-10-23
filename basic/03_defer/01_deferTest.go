ae76c1282fbce0cad8cce0acce4cc54445faa618
Zelayan <chenzezeya@gmail.com>
2022-10-23 23:04:20 +0800

    feat:add some defer case

--- basic/03_defer/01_deferTest.go
+++ basic/03_defer/01_deferTest.go
@@ -40,3 +40,83 @@ func f2() (i int) {
 	i = 1000
 	return i //1002
 }
+
+// 1. return defer 
+
+
+func deferFunc() int {
+    fmt.Println("defer func called")
+    return 0
+}
+
+func returnFunc() int {
+    fmt.Println("return func called")
+    return 0
+}
+
+func returnAndDefer() int {
+    defer deferFunc()
+    return returnFunc()
+}
+
+
+func DeferFunc1(i int) (t int) {
+    fmt.Println("t = ", t)
+    return 2
+}
+
+func returnButDefer() (t int) {  //t初始化0， 并且作用域为该函数全域
+    defer func() {
+        t = t * 10
+    }()
+
+    return 1
+}
+
+func defer_call() {
+    defer func() { fmt.Println("defer: panic 之前1") }()
+    defer func() { fmt.Println("defer: panic 之前2") }()
+
+    panic("异常内容")  //触发defer出栈
+
+	defer func() { fmt.Println("defer: panic 之后，永远执行不到") }()
+}
+
+func defer_call2() {
+
+    defer func() {
+        fmt.Println("defer: panic 之前1, 捕获异常")
+        if err := recover(); err != nil {
+            fmt.Println(err)
+        }
+    }()
+
+    defer func() { fmt.Println("defer: panic 之前2, 不捕获") }()
+
+    panic("异常内容")  //触发defer出栈
+
+	defer func() { fmt.Println("defer: panic 之后, 永远执行不到") }()
+}
+
+func panic2() {
+	defer func() {
+		if err := recover(); err != nil{
+			fmt.Println(err)
+		}else {
+			fmt.Println("fatal")
+		}
+	 }()
+ 
+	 defer func() {
+		 panic("defer panic")
+	 }()
+ 
+	 panic("panic")
+}
+
+func function(index int, value int) int {
+
+    fmt.Println(index)
+
+    return index
+}
---
+++ basic/03_defer/01_deferTest_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 有名函数返回值，在进入函数的时候初始化，并初始化为0值
+func TestDeferFunc1(t *testing.T) {
+	fmt.Printf("DeferFunc1(2): %v\n", DeferFunc1(2))
+	// 0
+	// 2
+}
+
+// defer遇上return
+// 先执行return，再执行defer
+func TestDeferAndReturn(t *testing.T) {
+	returnAndDefer()
+	// return func called
+	// defer func called
+}
+
+// 有名函数返回值遇上defer
+func TestDeferAndValueReturn(t *testing.T) {
+	fmt.Printf("returnButDefer(): %v\n", returnButDefer())
+}
+
+// defer遇上panic 不捕获
+func Test_defercall(t *testing.T) {
+	defer_call()
+}
+
+// defer遇上panic 捕获
+func Test_defercall2(t *testing.T) {
+	defer_call2()
+}
+
+// defer 中发生panic
+func Test_panic2(t *testing.T) {
+	panic2()
+	// defer panic
+}
+
+// 压zhan
+func Test_functionInDefer(t *testing.T) {
+	defer function(1, function(3, 0))
+    defer function(2, function(4, 0))
+}
